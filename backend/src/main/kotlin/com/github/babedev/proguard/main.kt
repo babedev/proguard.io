301e09e47203d5983b0b5684b2698dc214d66944
Christopher Ng <babedroid@gmail.com>
2018-01-31 21:05:05 +0700

    :rocket: Heroku preparation

--- backend/src/main/kotlin/com/github/babedev/proguard/main.kt
+++ backend/src/main/kotlin/com/github/babedev/proguard/main.kt
@@ -1,59 +1,58 @@
 package com.github.babedev.proguard
 
+import io.ktor.application.Application
 import io.ktor.application.call
+import io.ktor.application.install
+import io.ktor.features.CallLogging
+import io.ktor.features.DefaultHeaders
 import io.ktor.http.ContentType
 import io.ktor.response.respondText
+import io.ktor.routing.Routing
 import io.ktor.routing.get
-import io.ktor.routing.routing
-import io.ktor.server.engine.embeddedServer
-import io.ktor.server.netty.Netty
 import java.io.File
 
-fun main(args: Array<String>) {
+fun Application.main() {
+    install(DefaultHeaders)
+    install(CallLogging)
+    install(Routing) {
+        get("/") {
+            call.respondText("Hi", ContentType.Text.Html)
+        }
 
-    val server = embeddedServer(Netty, 8080) {
-        routing {
-            get("/") {
-                call.respondText("Hi", ContentType.Text.Html)
-            }
+        get("/{group}/{artifact}") {
+            val group = call.parameters["group"]
+            val artifact = call.parameters["artifact"]
 
-            get("/{group}/{artifact}") {
-                val group = call.parameters["group"]
-                val artifact = call.parameters["artifact"]
+            val proguardFolder = File("data/$group/$artifact")
 
-                val proguardFolder = File("backend/data/$group/$artifact")
-
-                val result = if (proguardFolder.exists() && proguardFolder.isDirectory) {
-                    proguardFolder
-                            .listFiles()
-                            .sortedByDescending { it.name }
-                            .firstOrNull()?.let {
-                        it.listFiles().firstOrNull()?.readText() ?: {
-                            "This library does not need Proguard or it is not exist in our system yet"
-                        }()
-                    } ?: { "This library does not need Proguard or it is not exist in our system yet" }()
-                } else "This library does not need Proguard or it is not exist in our system yet"
-
-                call.respondText(result, ContentType.Text.Plain)
-            }
-
-            get("/{group}/{artifact}/{version}") {
-                val group = call.parameters["group"]
-                val artifact = call.parameters["artifact"]
-                val version = call.parameters["version"]
-
-                val proguardFolder = File("backend/data/$group/$artifact/$version")
-
-                val result = if (proguardFolder.exists() && proguardFolder.isDirectory) {
-                    proguardFolder.listFiles().firstOrNull()?.readText() ?: {
+            val result = if (proguardFolder.exists() && proguardFolder.isDirectory) {
+                proguardFolder
+                        .listFiles()
+                        .sortedByDescending { it.name }
+                        .firstOrNull()?.let {
+                    it.listFiles().firstOrNull()?.readText() ?: {
                         "This library does not need Proguard or it is not exist in our system yet"
                     }()
-                } else "This library does not need Proguard or it is not exist in our system yet"
+                } ?: { "This library does not need Proguard or it is not exist in our system yet" }()
+            } else "This library does not need Proguard or it is not exist in our system yet"
 
-                call.respondText(result, ContentType.Text.Plain)
-            }
+            call.respondText(result, ContentType.Text.Plain)
+        }
+
+        get("/{group}/{artifact}/{version}") {
+            val group = call.parameters["group"]
+            val artifact = call.parameters["artifact"]
+            val version = call.parameters["version"]
+
+            val proguardFolder = File("backend/data/$group/$artifact/$version")
+
+            val result = if (proguardFolder.exists() && proguardFolder.isDirectory) {
+                proguardFolder.listFiles().firstOrNull()?.readText() ?: {
+                    "This library does not need Proguard or it is not exist in our system yet"
+                }()
+            } else "This library does not need Proguard or it is not exist in our system yet"
+
+            call.respondText(result, ContentType.Text.Plain)
         }
     }
-
-    server.start(true)
 }
